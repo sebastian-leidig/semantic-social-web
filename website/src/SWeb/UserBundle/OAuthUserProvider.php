55b7c512f31a6459b822d930d11dbb252215acc9
Sebastian Leidig <sebastian.leidig@gmail.com>
2014-11-25 01:26:05 +0530

    install hwi/oauth-bundle

---
+++ website/src/SWeb/UserBundle/OAuthUserProvider.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SWeb\UserBundle;
+
+use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthUserProvider as BaseOAuthUserProvider;
+
+
+class OAuthUserProvider extends BaseOAuthUserProvider { }
---
+++ website/src/SWeb/UserBundle/SWebUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SWeb\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SWebUserBundle extends Bundle
+{
+}
