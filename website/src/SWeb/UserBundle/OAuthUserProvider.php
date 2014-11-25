a91c0341aa8d742f9935ee33f2d9adaa1b108225
VServer <webmaster@sinnfragen.org>
2014-11-25 00:32:33 -0500

    working implementation of OAuth login

--- website/src/SWeb/UserBundle/OAuthUserProvider.php
+++ website/src/SWeb/UserBundle/OAuthUserProvider.php
@@ -3,6 +3,20 @@
 namespace SWeb\UserBundle;
 
 use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthUserProvider as BaseOAuthUserProvider;
+use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthUser;
 
 
-class OAuthUserProvider extends BaseOAuthUserProvider { }
+class OAuthUserProvider extends BaseOAuthUserProvider {
+
+    public function loadUserByOAuthUserResponse(UserResponseInterface $response)
+    {
+        $user_details = $response->getResponse();
+
+        if(!isset($user_details['hd']) || $user_details['hd'] !== "iiitd.ac.in")
+            return null; //TODO: Show error
+
+        $user = new OAuthUser($user_details['email']);
+        return $user;
+    }
+}
